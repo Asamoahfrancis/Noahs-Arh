e7695e82d2608c1722f5b2022ef64d9c89fcd784
Brefo Francis <brefofrancisasamaoh@gmail.com>
2023-08-07 22:09:49 +0000

    few changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        quintex: "url('/assest/logo.png')",
+        snniit: "url('/assest/snnit.png')",
+        venixx: "url('/assest/venix.png')",
+        paraIcon: "url('/assest/para_icon.png')",
+        zindexI: "url('/assest/zindex.png')",
+        parabolic: "url('/assest/Parabolic.png')",
+        pedmas: "url('/assest/Pedmas.png')",
+        mainImg: "url('/assest/Rectangle.png')",
+        buyingImg: "url('/assest/buyingImg.png')",
+        sellingImg: "url('/assest/SellingImg.png')",
+        consultionImg: "url('/assest/consultationImg.png')",
+        propertyImg: "url('/assest/PropertyManagement.png')",
+      },
+    },
   },
   plugins: [],
 };
